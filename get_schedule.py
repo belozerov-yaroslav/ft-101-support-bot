f08d7df9bff238bf4ed1001514ae38052594cc61
sw1ftin <jornysh@gmail.com>
2023-10-06 18:49:19 +0500

    included get_schedule.py to index.py

--- get_schedule.py
+++ get_schedule.py
@@ -1,25 +1,36 @@
+import requests
 import datetime
 from datetime import datetime as dt
 import csv
 import json
 from tabulate import tabulate
+from tg_module import send_message
+import os.path
 
 
-def get_schedule():
+def get_schedule_from_url():
+    with open("tokens.json", "r") as tokens:
+        url = json.load(tokens)["schedule_url"]
+        open("schedules/schedule.csv", "wb").write(requests.get(url).content)
+
+
+def get_schedule(today):
     with open("tokens.json", "r", encoding="utf8") as tokens:
         tokens_loaded = json.load(tokens)
         timezone = datetime.timezone(datetime.timedelta(hours=int(tokens_loaded["timezone_delta"])))
-        today = dt.now(tz=timezone).weekday()
+        today_weekday = today.weekday()
         is_even_week = ((dt.now(tz=timezone) - dt(year=2023, month=9, day=4, tzinfo=timezone)).days // 7 + 1) % 2 == 0
 
         with open('schedules/schedule.csv', newline='', encoding='utf-8') as schedule:
-            if today < 5:
+            if today_weekday < 5:
                 schedule_reader = [row for row in csv.reader(schedule, delimiter=',')]
                 output_schedule = []
 
-                for row_index in range(2 + today * 12, 14 + today * 12, 2):
+                for row_index in range(2 + today_weekday * 12, 14 + today_weekday * 12, 2):
                     lesson_number = schedule_reader[row_index][1]
-                    row = schedule_reader[row_index + is_even_week][2:4] # [2:4] - только для 1 группы
+
+                    # [2:4] - только для 1 группы
+                    row = schedule_reader[row_index + is_even_week][2:4]
 
                     row[0] = row[0].replace('\n', ' ')
                     if not row[0]: row[0] = '-'
@@ -33,4 +44,14 @@ def get_schedule():
 
                 return tabulate(output_schedule)
 
-            return -1
+            return ""
+
+
+def send_schedule(today):
+    if (today.weekday() == 6) or (not os.path.exists("schedules/schedule.csv")):
+        get_schedule_from_url()
+
+    schedule_message = get_schedule(today)
+
+    if schedule_message:
+        send_message(schedule_message)
--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from matan_schedule import send_matan_schedule
 import echo_mode
 from checkBirthday import checkBirthday
 from lists import asia_ekat
+from get_schedule import send_schedule
 
 def handler(event, context):
     today = datetime.datetime.now(tz=asia_ekat)
@@ -14,8 +15,11 @@ def handler(event, context):
                                   month=int(parametrs['month']),
                                   year=int(parametrs['year']),
                                   tzinfo=asia_ekat)
+
     send_matan_schedule(today)
     checkBirthday(today)
+    send_schedule(today)
+
     return {
         'statusCode': 200,
         'body': 'ok!',
