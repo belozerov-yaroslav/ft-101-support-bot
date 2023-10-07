a8ae303bcf1a94b8e47bef4f29d7f29981583b60
belozerov-yaroslav <jarblz@yandex.ru>
2023-10-07 20:27:20 +0500

    Убрал файл с секретами, настроил на работ с переменными окружения

--- echo_mode.py
+++ echo_mode.py
@@ -1,6 +1,6 @@
-from secrets import ECHO_CHAT
 from tg_module import send_message
+import os
 
 
 def echo():
-    send_message("Function is triggered!", chat_id=ECHO_CHAT)
+    send_message("Function is triggered!", chat_id=os.environ.get("ECHO_CHAT_ID"))
--- tg_module.py
+++ tg_module.py
@@ -1,11 +1,11 @@
-from secrets import TG_CHAT_ID, TOKEN
 import requests
+import os
 
 
-def send_message(message_text, chat_id=TG_CHAT_ID, turn_on_markdown=False):
+def send_message(message_text, chat_id=os.environ.get('TG_CHAT_ID'), turn_on_markdown=False):
     json_params = {"chat_id": chat_id,
                    "text": message_text}
     if turn_on_markdown:
         json_params['parse_mode'] = "MarkdownV2"
-    response_text = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    response_text = requests.post(f"https://api.telegram.org/bot{os.environ.get('TG_TOKEN')}/sendMessage",
                                   json=json_params).text
