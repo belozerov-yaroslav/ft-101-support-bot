9b1c5e52b704df1783e8ef7c5fc50affe22156dd
belozerov-yaroslav <jarblz@yandex.ru>
2024-10-05 19:39:34 +0500

    обнова расписания

--- ScheduleTests.py
+++ ScheduleTests.py
@@ -2,5 +2,5 @@ import get_schedule
 import datetime as dt
 
 
-for i in get_schedule.get_schedule(dt.datetime(year=2024, month=9, day=25, tzinfo=dt.timezone(dt.timedelta(hours=5)))):
+for i in get_schedule.get_schedule(dt.datetime(year=2024, month=10, day=1, tzinfo=dt.timezone(dt.timedelta(hours=5)))):
     print(i)
